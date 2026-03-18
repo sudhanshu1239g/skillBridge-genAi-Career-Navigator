f733f7d089112d58e88432ff84efa49a0a2bbbcf
sudhanshu <gsam1239h@gmail.com>
2026-03-19 02:00:20 +0530

    frontend-initialize

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import { RouterProvider } from "react-router"
+import { router } from "./app.routes.jsx"
+
+function App() {
+
+  return (
+    <>
+    <RouterProvider router={router}/>
+    </>
+   
+  )
+}
+
+export default App
---
+++ frontend/src/app.routes.jsx
@@ -0,0 +1,15 @@
+import {createBrowserRouter} from 'react-router'
+import Register from './features/auth/pages/register'
+import Login from './features/auth/pages/login'
+
+export const router=createBrowserRouter([
+    {
+        path:'/register',
+        element:<Register/>
+    },
+    {   path:'/login',
+        element:<Login/>
+    }
+])
+
+

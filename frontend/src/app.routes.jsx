3b3780641313baabc277b251be7d17c2bda338f3
sudhanshu <gsam1239h@gmail.com>
2026-03-19 09:06:02 +0530

    Implement login and registration forms with styling and navigation

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,6 +1,6 @@
-import {createBrowserRouter} from 'react-router'
-import Register from './features/auth/pages/register'
-import Login from './features/auth/pages/login'
+import {createBrowserRouter} from 'react-router';
+import Register from './features/auth/pages/register';
+import Login from './features/auth/pages/login';
 
 export const router=createBrowserRouter([
     {
--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import '../auth.form.scss'
+import { useNavigate,Link } from 'react-router'
 
 const Login = () => {
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+    }
+
   return (
-    <div>
-      Login
-    </div>
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+
+            <form onSubmit={handleSubmit}>
+
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder='enter email' />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder='enter password' required />
+                </div>
+
+                <button className='button primary-button'>Login</button>
+
+            </form>
+
+            <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
+
+        </div>
+
+    </main>
   )
 }
 

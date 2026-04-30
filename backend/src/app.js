b395c8fa552edee13545099196efa96322a73646
sudhanshu <gsam1239h@gmail.com>
2026-04-30 23:16:44 +0530

    final

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 
 app.use(cors({
-    origin: "skill-bridge-gen-ai-career-navigato-indol.vercel.app",
+    origin: "https://skill-bridge-gen-ai-career-navigato-indol.vercel.app/",
     credentials: true
 }))
 app.use(express.json())

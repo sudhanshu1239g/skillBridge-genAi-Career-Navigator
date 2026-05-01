1a07c78207b070eba8212fd003aad2ce173d829a
sudhanshu <gsam1239h@gmail.com>
2026-05-01 08:41:05 +0530

    initial ui

--- frontend/src/features/auth/components/Protected.jsx
+++ frontend/src/features/auth/components/Protected.jsx
@@ -6,8 +6,12 @@ const Protected = ({children}) => {
     const { loading,user } = useAuth()
 
 
-    if(loading){
-        return (<main><h1>Loading...</h1></main>)
+    if (loading) {
+        return (
+            <main className='loading-screen'>
+                <h1>Loading...</h1>
+            </main>
+        )
     }
 
     if(!user){
